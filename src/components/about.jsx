0b7c995ea9b1a16b3edee20fadc9b7a7f99d94de
alfredmaurya <37591173+alfredmaurya@users.noreply.github.com>
2019-07-29 22:03:41 +0530

    Add files via upload

---
+++ src/components/about.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>ABOUT...........</p>
+        <p>Bhamma pichi pilakayi </p>
+      </div>  
+    );
+  }
+}
+
+export default About;
